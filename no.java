56511c411da753b982db7e233ca700c9f2269eab
sandhikavi <sandhyav.velusamy@gmail.com>
2016-08-16 18:32:20 +0530

    Update and rename .gitignore to no.java

---
+++ no.java
@@ -0,0 +1,36 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+
+class no
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+int i;
+System.out.println("Enter the number" +i);		
+f(i%2==0)
+{
+
+System.out.println("the number is even");
+
+}
+else
+
+{
+
+System.out.println("the number is odd");
+
+}	
+}
+
+}
